781139552dd7bbf2f9ffeebbd7c32b446b542597
Abutler101 <alex@Alex.home>
2017-07-14 22:17:47 +0100

    Basic body

--- sketch.js
+++ sketch.js
@@ -1,7 +1,13 @@
+var nodes = [3,5,9,4,6,2];
 function setup() {
-
+  createCanvas(800,800);
 }
 
 function draw() {
-
+  background(81);
+  for(var i =0;i<nodes.length;i++){
+    for(var j =0;j<nodes[i];i++){
+      //DRAW A CIRCLE
+    }
+  }
 }
