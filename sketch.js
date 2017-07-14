8ce1c47535dfc2c95d7ee8767acde08e83c3aa43
Abutler101 <alex@Alex.home>
2017-07-14 22:52:09 +0100

    Basic body

--- sketch.js
+++ sketch.js
@@ -1,13 +1,35 @@
 var nodes = [3,5,9,4,6,2];
+var nodePos = [];
+var NodeWidth = 50;
 function setup() {
-  createCanvas(800,800);
+  createCanvas(1000,700);
+  var x = 40;
+  for(var i =0;i<nodes.length;i++){
+    var y = 40;
+    var layer = [];
+    for(var j =0;j<nodes[i];j++){
+      layer.push([x,y]);
+      y+=75;
+    }
+    nodePos.push(layer);
+    x+=150;
+  }
 }
 
 function draw() {
   background(81);
-  for(var i =0;i<nodes.length;i++){
-    for(var j =0;j<nodes[i];i++){
-      //DRAW A CIRCLE
+  for(var i =0;i<nodePos.length;i++){
+    for(var j =0;j<nodePos[i].length;j++){
+      ellipse(nodePos[i][j][0],nodePos[i][j][1],NodeWidth);
     }
   }
+  debugger;
+  for(var i =0;i<nodePos.length;i++){
+    for(var j =0;j<nodePos[i].length;j++){
+      for(var k=1;k<nodes[k];k++){
+
+      }
+    }
+  }
+
 }
