3f224136b1714cd1bc6b58c555db13b0c4c7249b
Abutler101 <alex@Alex.home>
2017-07-15 13:59:21 +0100

    Added values

--- sketch.js
+++ sketch.js
@@ -1,9 +1,10 @@
-var nodes = [3,5,9,4,6,2];
+var nodes = [10,5,9,4,6,2];
 var nodePos = [];
 var NodeWidth = 50;
+var outOfLayers = [[],[],[],[],[],[]];
 function setup() {
-  createCanvas(1000,700);
-  var x = 40;
+  createCanvas(1500,800);
+  var x = 100;
   for(var i =0;i<nodes.length;i++){
     var y = 40;
     var layer = [];
@@ -17,13 +18,22 @@ function setup() {
 }
 
 function draw() {
+  frameRate(0.6);
+  //For testing purposes only
+  for(var l = 0;l<nodes.length;l++){
+    for(var m =0;m<nodes[l];m++){
+      outOfLayers[l][m] = random(-1,1);
+    }
+  }
+  //End testing only segment
   background(81);
   for(var i =0;i<nodePos.length;i++){
     for(var j =0;j<nodePos[i].length;j++){
       ellipse(nodePos[i][j][0],nodePos[i][j][1],NodeWidth);
+      var val = round(outOfLayers[i][j]*100)/100;
+      text(val,nodePos[i][j][0]-20,nodePos[i][j][1]);
     }
   }
-//  debugger;
   for(var i =0;i<nodePos.length;i++){
     for(var j =0;j<nodePos[i].length;j++){
       if(i!=nodePos.length-1){
@@ -33,5 +43,4 @@ function draw() {
       }
     }
   }
-
 }
